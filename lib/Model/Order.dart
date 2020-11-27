ca57581b51bde0a953db766ac8f1df80a530c9e6
cysko7927 <cysko7927@debcysko.it>
2020-11-27 21:46:58 +0100

    Added class order

---
+++ lib/Model/Order.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+import 'package:like_eat/Model/User.dart';
+
+class Order {
+  DateTime _time;
+  User _user;
+
+  Order(
+    this._time,
+    this._user,
+  );
+
+  //Getter
+
+  //Setter
+
+  //Utility
+
+}
