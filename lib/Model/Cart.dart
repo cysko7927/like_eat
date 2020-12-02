cda66a44c682300a588dde99cd0ce932290f7ebb
cysko7927 <cysko7927@debcysko.it>
2020-12-02 16:12:49 +0100

    Cart updated

--- lib/Model/Cart.dart
+++ lib/Model/Cart.dart
@@ -1,1 +1,39 @@
-class Cart {}
+import 'package:like_eat/Model/Product.dart';
+import 'package:like_eat/Model/User.dart';
+
+class Cart {
+  User _user;
+  Map<Product, int> _productsAndQuantity;
+
+  Cart(this._user) {
+    _productsAndQuantity = {};
+  }
+
+  //Getter
+
+  User get user => _user;
+  Map<Product, int> get productAndQuantity => createMapProduct();
+
+  //Setter
+  void addProductAndQuantity(Product product, int quantity) {
+    _productsAndQuantity[product] = quantity;
+  }
+
+  void removeProductAndQuantity(Product product, int quantity) {
+    int qua = _productsAndQuantity[product];
+
+    if (qua ==
+        quantity) //If the quantity in the input is equal at that in the cart
+      _productsAndQuantity.remove(product); //remove the product from the map
+    else
+      _productsAndQuantity[product] = qua - quantity; //Else update the quantity
+  }
+
+  //Utility
+
+  Map<Product, int> createMapProduct() {
+    Map<Product, int> map = new Map<Product, int>();
+    map.addAll(_productsAndQuantity);
+    return map;
+  }
+}
