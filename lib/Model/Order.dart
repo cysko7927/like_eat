e5ed1db69d33402e341503986dd875f51d013d8f
cysko7927 <cysko7927@debcysko.it>
2020-12-02 16:13:10 +0100

    Order updated

--- lib/Model/Order.dart
+++ lib/Model/Order.dart
@@ -1,20 +1,30 @@
 import 'dart:convert';
 
+import 'package:like_eat/Model/Product.dart';
 import 'package:like_eat/Model/User.dart';
 
 class Order {
   DateTime _time;
   User _user;
+  Map<Product, int> _productAndQuantity;
 
-  Order(
-    this._time,
-    this._user,
-  );
+  Order(this._time, this._user, Map<Product, int> mapCart) {
+    _productAndQuantity = {};
+    _productAndQuantity.addAll(mapCart);
+  }
 
   //Getter
+  DateTime get time => _time;
+  User get user => _user;
+  Map<Product, int> get productAndQuantity => createMapProduct();
 
   //Setter
 
   //Utility
 
+  Map<Product, int> createMapProduct() {
+    Map<Product, int> map = new Map<Product, int>();
+    map.addAll(_productAndQuantity);
+    return map;
+  }
 }
