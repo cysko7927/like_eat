6e522a342d0893624f15d0824c1dffe7c3d18b2c
cysko7927 <cysko7927@debcysko.it>
2020-12-09 19:24:27 +0100

    First Version of the Product Manager

---
+++ lib/ViewModel/NotificationManager.dart
@@ -0,0 +1,17 @@
+import 'dart:ffi';
+
+import 'package:flutter/cupertino.dart';
+import 'package:like_eat/Model/Notification.dart';
+import 'package:like_eat/ViewModel/WebService.dart';
+
+class NotificationManager extends ChangeNotifier {
+  List<Notifications> _notifications = [];
+
+  Future<void> getNotifications(String nickname) async {
+    List<Notifications> result = await Webservice()
+        .fetchNotifications(nickname); //Obtain the notifications of a user
+
+    _notifications = result;
+    notifyListeners(); //Commit the state to the view
+  }
+}
