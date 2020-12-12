b3b03c7ef39788b3db244e8eed08de6baa1e5572
cysko7927 <cysko7927@debcysko.it>
2020-12-12 16:32:38 +0100

    added operations

--- lib/ViewModel/CartManager.dart
+++ lib/ViewModel/CartManager.dart
@@ -1,15 +1,42 @@
 import 'package:flutter/cupertino.dart';
 import 'package:like_eat/Model/Cart.dart';
+import 'package:like_eat/Model/Product.dart';
+import 'package:like_eat/ViewModel/ViewModel.dart';
 import 'package:like_eat/ViewModel/WebService.dart';
 
 class CartManager extends ChangeNotifier {
   Cart _cart;
 
-  void insertProductInTheCart(String name, String type, String supplier,
-      String nickname, int quantity) async {
-    Webservice().postCart(name, type, supplier, nickname, quantity);
+  void obtainCart() async {
+    _cart =
+        await Webservice().fetchCart(ViewModel.userDataManager.user.nickname);
+    notifyListeners();
   }
 
-  //ToDo
-  //Fare operazioni mostra carrello  e rimuovi oggetti dal carrello
+  void insertProductInTheCart(
+      String name, String type, String supplier, int quantity) async {
+    Webservice().postAddCart(name, type, supplier,
+        ViewModel.userDataManager.user.nickname, quantity);
+    obtainCart();
+    notifyListeners();
+  }
+
+  void removeProductInTheCart(int index) {
+    Product selected = _cart.productsInCart.elementAt(index);
+    Webservice().postRemoveCart(selected.name, selected.type, selected.type,
+        ViewModel.userDataManager.user.nickname);
+
+    obtainCart();
+    notifyListeners();
+  }
+
+  void updateQuanityCart(int index, int quantity) {
+    Product selected = _cart.productsInCart.elementAt(index);
+
+    Webservice().postUpdateCart(selected.name, selected.type, selected.type,
+        ViewModel.userDataManager.user.nickname, quantity);
+
+    obtainCart();
+    notifyListeners();
+  }
 }
