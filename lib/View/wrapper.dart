c0529175c8da9ef2010b4e69c2cc51e5ca29330b
cysko7927 <cysko7927@gmail.com>
2021-03-03 18:24:36 +0100

    Added Wrapper and Authenticate

---
+++ lib/View/wrapper.dart
@@ -0,0 +1,21 @@
+import 'dart:js';
+
+import 'package:flutter/material.dart';
+import 'package:like_eat/View/HomePageView.dart';
+import 'package:provider/provider.dart';
+import 'package:like_eat/Model/User.dart';
+import 'package:like_eat/View/Authenticate.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<UserAnon>(
+        context); //Obtain the state of the user logged from the provider
+
+    if (user == null) {
+      return Authenticate();
+    } else {
+      return HomePage();
+    }
+  }
+}
