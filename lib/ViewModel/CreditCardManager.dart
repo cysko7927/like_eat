0ed7a19c86390db3a7bf49d35592670ca03992be
cysko7927 <cysko7927@gmail.com>
2021-03-17 16:15:02 +0100

    Written the query and the stream

--- lib/ViewModel/CreditCardManager.dart
+++ lib/ViewModel/CreditCardManager.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:like_eat/Model/CreditCard.dart';
 import 'package:like_eat/ViewModel/ViewModel.dart';
@@ -9,38 +10,79 @@ import 'package:like_eat/ViewModel/WebService.dart';
  * The single Credit Card is immutable
  */
 class CreditCardManager extends ChangeNotifier {
+  final String uidUser; //UID of the user whose addresses you want
+  CollectionReference creditCardsReference =
+      FirebaseFirestore.instance.collection('CreditCard'); //Reference to DB
   List<CreditCard> _listOfCreditCards = [];
 
+  CreditCardManager(this.uidUser);
+
+  /**
+   * Obtain the list of Credit Card from the DB and save it inside the object
+   */
   void obtainCreditCards() async {
-    _listOfCreditCards = await Webservice()
-        .fetchCreditCards(ViewModel.userDataManager.user.nickname);
+    QuerySnapshot result =
+        await creditCardsReference.where('uidUser', isEqualTo: uidUser).get();
 
-    notifyListeners();
+    _listOfCreditCards = _creditCardListFromSnapshot(result);
   }
 
+  //Add a new Credit Card in the DB for the user
   void addCreditCard(String number, String cvc, String expDate) {
-    Webservice().postAddCreditCard(
-        //Add the new Credit Card
-        number,
-        cvc,
-        expDate,
-        ViewModel.userDataManager.user.nickname);
+    //Add the new CreditCard in the DB
+    creditCardsReference.add({
+      'number': number,
+      'cvc': cvc,
+      'expDate': expDate,
+      'uidUser': uidUser,
+    });
+
     obtainCreditCards(); //Update the state
-    notifyListeners(); //Notify the view
   }
 
-  void deleteCreditCard(int index) {
-    CreditCard selected = _listOfCreditCards.elementAt(index);
+  /**
+   * Remove a Credit Card from the DB using the index in input
+   * Precondition: the selected Credit Card must be exist in the DB
+   */
+  void deleteCreditCard(int index) async {
+    CreditCard selected = _listOfCreditCards
+        .elementAt(index); //Take the credit card from the local list
 
-    Webservice().postRemoveCreditCard(
-        selected.number,
-        selected.cvc,
-        selected.expDate,
-        ViewModel
-            .userDataManager.user.nickname); //Remove the selected Credit Card
+    //obtain the reference to the credit cards to remove
+    QuerySnapshot result = await creditCardsReference
+        .where('uidUser', isEqualTo: uidUser)
+        .where('number', isEqualTo: selected.number)
+        .where('cvc', isEqualTo: selected.cvc)
+        .where('expDate', isEqualTo: selected.expDate)
+        .get(); //obtain the reference of the selected credits cards from DB
 
-    obtainCreditCards(); //Update the state
-    notifyListeners(); //Notify the view
+    creditCardsReference
+        .doc(result.docs.elementAt(0).id)
+        .delete(); //Remove the selected credit cards to DB
+
+    obtainCreditCards(); //Update the list of the credit cards inside the object
+  }
+
+  //obtain the stream of the list of credit cards of the user with uid = uidUser from this class
+  Stream<List<CreditCard>> get creditCardsStream {
+    obtainCreditCards(); //Update the list of credit Cards inside the object
+    return creditCardsReference
+        .where('uidUser', isEqualTo: uidUser)
+        .snapshots()
+        .map(_creditCardListFromSnapshot);
+  }
+
+  //Convert The querySnapshot that contains the credit Cards in a list
+  //that contains those Credit cards
+  //Precondition: The snapshot must contain the credit cards with the correct parameters
+  List<CreditCard> _creditCardListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.docs
+        .map((doc) => CreditCard(
+              doc.data()['number'] ?? '',
+              doc.data()['cvc'] ?? '',
+              doc.data()['expDate'] ?? '',
+            ))
+        .toList();
   }
 
   get creditCards => _listOfCreditCards;
