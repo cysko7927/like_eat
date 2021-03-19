cc2aa0bcb40fb475031f6b7bd4d236d6511cee7e
matteocecini <matteo.cecini@mail.polimi.it>
2021-03-19 18:04:19 +0100

    CatalogView

--- lib/View/Wrapper/HomePage/Catalog/CatalogView.dart
+++ lib/View/Wrapper/HomePage/Catalog/CatalogView.dart
@@ -1,31 +1,154 @@
 import 'package:flutter/material.dart';
+import 'package:like_eat/Model/Product.dart';
 import 'package:like_eat/View/Wrapper/HomePage/Catalog/ProductList.dart';
 
+import 'package:direct_select/direct_select.dart';
+import 'package:like_eat/ViewModel/SearchManager.dart';
+
 class Catalog extends StatefulWidget {
-   @override
+  @override
   _CatalogState createState() => _CatalogState();
 }
-class _CatalogState extends State<Catalog>{
+
+class _CatalogState extends State<Catalog> {
   @override
+  TextEditingController searchController = TextEditingController();
+  String search = '';
+  bool _available = false;
+  List<Product> products=new List<Product>();
+
+  SearchManager searchManager = new SearchManager();
+
+  final elements = [
+    "Cereal",
+    "Fruit",
+    "Legumes",
+    "casuale",
+  ];
+  int selectedIndex = 0;
+
+  List<Widget> _buildItems() {
+    return elements
+        .map((val) => MySelectionItem(
+              title: val,
+            ))
+        .toList();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        title: Text('Catalog'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.settings,
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: Text('Catalog'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.settings,
+              ),
+              onPressed: () {
+                Navigator.pushNamed(context, 'Setting');
+              },
+            )
+          ],
+        ),
+        body: SingleChildScrollView(
+            child: Column(
+              
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(15.0),
+              child: TextField(
+              
+              controller: searchController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Search Product',
+              ),
+              onChanged: (text) {
+                setState(() {
+                  search = text;
+                  searchManager.searchProduct(search,elements[selectedIndex],_available);
+                  products=new List<Product>();
+                  products.addAll(searchManager.products);
+                  
+                });
+              },
             ),
-            onPressed: () {
-              Navigator.pushNamed(context, 'Setting');
-            },
-          )
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Expanded(child: SizedBox(height: 200.0, child: ProductList()),
-      ), 
-    ));
+            ),
+            Row(
+              children: [
+                Checkbox(
+                    value: _available,
+                    onChanged: (value) {
+                      setState(() {
+                        _available = value;
+                      });
+                    }),
+                Expanded(
+                    child: Container(
+                        child: Text("Search only available product"))),
+              ],
+            ),
+            DirectSelect(
+                    itemExtent: 50.0,
+                    selectedIndex: selectedIndex,
+                    child: MySelectionItem(
+                      isForList: false,
+                      title: elements[selectedIndex],
+                    ),
+                    onSelectedItemChanged: (index) {
+                      setState(() {
+                        selectedIndex = index;
+                      });
+                    },
+                    items: _buildItems()),
+            Container(
+              child: SizedBox(height: 200.0, child: ProductList(products)),
+            ),
+          ],
+        )));
   }
 }
+
+class MySelectionItem extends StatelessWidget {
+  final String title;
+  final bool isForList;
+
+  const MySelectionItem({Key key, this.title, this.isForList = true})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 100.0,
+      child: isForList
+          ? Padding(
+              child: _buildItem(context),
+              padding: EdgeInsets.all(10.0),
+            )
+          : Card(
+              margin: EdgeInsets.symmetric(horizontal: 10.0),
+              child: Stack(
+                children: <Widget>[
+                  _buildItem(context),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Icon(Icons.arrow_drop_down),
+                  )
+                ],
+              ),
+            ),
+    );
+  }
+  _buildItem(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      alignment: Alignment.center,
+      child: Text(title),
+    );
+  }
+}
+
+
--- lib/View/Wrapper/HomePage/Catalog/ProductList.dart
+++ lib/View/Wrapper/HomePage/Catalog/ProductList.dart
@@ -2,17 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:like_eat/Model/Product.dart';
 
 import 'package:like_eat/View/Wrapper/HomePage/Catalog/ProductTile.dart';
-import 'package:provider/provider.dart';
 
 class ProductList extends StatefulWidget {
+  final List<Product> products;
+  ProductList(this.products);
+
   @override
   _ProductListState createState() => _ProductListState();
 }
 
 class _ProductListState extends State<ProductList> {
   @override
+
   Widget build(BuildContext context) {
-    final products = Provider.of<List<Product>>(context);
+    List<Product> products = widget.products;
 
     return ListView.builder(
         itemCount: products.length,
