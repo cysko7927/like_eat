8d3ea9ce3e1a33c7be70eedf8d476ffb2dd23192
cysko7927 <cysko7927@gmail.com>
2021-03-29 18:15:50 +0200

    Written query for the cart and product observed

--- lib/ViewModel/CartManager.dart
+++ lib/ViewModel/CartManager.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:like_eat/Model/Cart.dart';
 import 'package:like_eat/Model/Product.dart';
@@ -6,41 +7,120 @@ import 'package:like_eat/ViewModel/WebService.dart';
 
 class CartManager extends ChangeNotifier {
   Cart _cart;
+  CollectionReference cartReference =
+      FirebaseFirestore.instance.collection('Cart-Products');
+  CollectionReference productsReference =
+      FirebaseFirestore.instance.collection('Product');
+  String _uid;
 
-  void obtainCart() async {
-    _cart =
-        await Webservice().fetchCart(ViewModel.userDataManager.user.nickname);
-    notifyListeners();
-  }
+  CartManager(this._uid);
 
-  void insertProductInTheCart(
+  void obtainCart() async {}
+
+  /*void insertProductInTheCart(
       String name, String type, String supplier, int quantity) async {
     Webservice().postAddCart(name, type, supplier,
         ViewModel.userDataManager.user.nickname, quantity);
     obtainCart();
     notifyListeners();
+  }*/ //Per il momento non serve
+
+  void removeProductInTheCart(Product product) async {
+    //obtain the reference to the product in the Cart to remove from the DB
+    QuerySnapshot result = await cartReference
+        .where('name', isEqualTo: product.name)
+        .where('price', isEqualTo: product.price)
+        .where('quantity', isEqualTo: product.quantity)
+        .where('supplier', isEqualTo: product.supplier)
+        .where('type', isEqualTo: product.type)
+        .where('uid', isEqualTo: _uid)
+        .get();
+
+    cartReference
+        .doc(result.docs.elementAt(0).id)
+        .delete(); //Remove the selected product from the cart interacting with the DB
+
+    result = await productsReference
+        .where('name', isEqualTo: product.name)
+        .where('type', isEqualTo: product.type)
+        .where('supplier', isEqualTo: product.supplier)
+        .get(); //obtain the reference of the product from DB
+
+    productsReference.doc(result.docs.elementAt(0).id).update({
+      'quantity': product.quantity + result.docs.elementAt(0).data()['quantity']
+    }); //Update the quantity of the product in the DB(quantity in the cart + residual quantity in the magazine )
   }
 
-  void removeProductInTheCart(int index) {
-    Product selected = _cart.productsInCart.elementAt(index);
-    Webservice().postRemoveCart(selected.name, selected.type, selected.type,
-        ViewModel.userDataManager.user.nickname);
+  /**
+   * Update the quantity of the product in the cart in input,the quantity in input is the 
+   * quantity to add(positive) or to remove (negative)
+   * Precondition:The quantity in input must be correct and it must not lead 
+   * the quantity in the Magazine to be negative and -product.quantity < quantity <= quantity in the magazine
+   */
+  void updateQuantityCart(Product product, int quantity) async {
+    QuerySnapshot productMagazine = await productsReference
+        .where('name', isEqualTo: product.name)
+        .where('type', isEqualTo: product.type)
+        .where('supplier', isEqualTo: product.supplier)
+        .get(); //obtain the reference of the product from DB(Magazine)
 
-    obtainCart();
-    notifyListeners();
+    QuerySnapshot productCart = await cartReference
+        .where('name', isEqualTo: product.name)
+        .where('price', isEqualTo: product.price)
+        .where('quantity', isEqualTo: product.quantity)
+        .where('supplier', isEqualTo: product.supplier)
+        .where('type', isEqualTo: product.type)
+        .where('uid', isEqualTo: _uid)
+        .get(); //obtain the reference of the product from DB(Cart)
+
+    num quantityMagazine = productMagazine.docs.elementAt(0).data()['quantity'];
+
+    productsReference.doc(productMagazine.docs.elementAt(0).id).update({
+      'quantity': quantityMagazine - quantity
+    }); //Update the quantity of the product in the DB(residual quantity in the magazine -quantity in the cart)
+
+    cartReference.doc(productCart.docs.elementAt(0).id).update({
+      'quantity': productCart.docs.elementAt(0).data()['quantity'] + quantity
+    }); //Update the quantity of the product in the Cart in the DB(quantity in the cart + quantity added)
   }
 
-  void updateQuanityCart(int index, int quantity) {
-    Product selected = _cart.productsInCart.elementAt(index);
-
-    Webservice().postUpdateCart(selected.name, selected.type, selected.type,
-        ViewModel.userDataManager.user.nickname, quantity);
-
-    obtainCart();
-    notifyListeners();
+  //obtain the stream of the list of the products in the cart of the user with uid = _uid from this class
+  Stream<List<Product>> get productsInCart {
+    return cartReference
+        .where('uidUser', isEqualTo: _uid)
+        .snapshots()
+        .map(_productsInTheCartListFromSnapshot);
   }
 
-  //Serve un metodo per ottenere i prodotti e la quantità del carello
+  //Convert The querySnapshot that contains the products in the cart of a user in a list
+  //that contains those products
+  //Precondition: The snapshot must contain the products with the correct parameters
+  List<Product> _productsInTheCartListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.docs
+        .map((doc) => Product(
+              doc.data()['supplier'] ?? '',
+              doc.data()['type'] ?? '',
+              doc.data()['name'] ?? '',
+              doc.data()['price'] ?? '',
+              doc.data()['quantity'] ?? '',
+            ))
+        .toList();
+  }
+
+  /**
+   * Returns the quantity of a product from the magazine
+   */
+  Future<num> obtainQuantityMagazineOfAProduct(Product product) async {
+    QuerySnapshot result = await productsReference
+        .where('name', isEqualTo: product.name)
+        .where('type', isEqualTo: product.type)
+        .where('supplier', isEqualTo: product.supplier)
+        .get(); //obtain the reference of the product from DB
+
+    return result.docs
+        .elementAt(0)
+        .data()['quantity']; //obtain the quantity in magazine
+  }
 
   get allTheCart => _cart.productAndQuantity;
 }
--- lib/ViewModel/ObservedProductManager.dart
+++ lib/ViewModel/ObservedProductManager.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:like_eat/Model/Product.dart';
 import 'package:like_eat/ViewModel/ViewModel.dart';
@@ -5,18 +6,60 @@ import 'package:like_eat/ViewModel/WebService.dart';
 
 class ObservedProductManager extends ChangeNotifier {
   List<Product> _observedproducts;
+  CollectionReference productsObservedReference =
+      FirebaseFirestore.instance.collection('User-ProductObserved');
+  CollectionReference productsReference =
+      FirebaseFirestore.instance.collection('Product');
+  String _uid;
 
-  void obtainObservedProduct() async {
-    _observedproducts = await Webservice()
-        .fetchObservedProducts(ViewModel.userDataManager.user.nickname);
-    notifyListeners();
+  ObservedProductManager(this._uid);
+
+  //void obtainObservedProduct() async {} Non serve per il momento
+
+  void insertOrRemoveProduct(Product product, bool observed) async {
+    if (observed) {
+      QuerySnapshot result = await productsObservedReference
+          .where('nameProduct', isEqualTo: product.name)
+          .where('typeProduct', isEqualTo: product.type)
+          .where('supplierProduct', isEqualTo: product.supplier)
+          .where('uid', isEqualTo: _uid)
+          .get(); //obtain the reference of the product observed from DB
+
+      productsReference
+          .doc(result.docs.elementAt(0).id)
+          .delete(); //Delete the product observed from the list in the DB
+
+    } else {
+      productsObservedReference.add({
+        'nameProduct': product.name,
+        'supplierProduct': product.supplier,
+        'typeProduct': product.type,
+        'uid': _uid,
+      }); //Add the product in the list of observed product of the user in the DB
+
+    }
   }
 
-  void insertOrRemoveProduct(String name, String type, String supplier,
-      String nickname, bool observed) {
-    Webservice().postFlagObservable(name, type, supplier,
-        ViewModel.userDataManager.user.nickname, observed);
-    obtainObservedProduct();
-    notifyListeners();
+  //obtain the stream of the list of the observed products of the user with uid = _uid from this class
+  Stream<List<Product>> get productsObserved {
+    return productsObservedReference
+        .where('uidUser', isEqualTo: _uid)
+        .snapshots()
+        .map(_productsObservedListFromSnapshot);
+  }
+
+  //Convert The querySnapshot that contains the observed products of a user in a list
+  //that contains those products
+  //Precondition: The snapshot must contain the products with the correct parameters
+  List<Product> _productsObservedListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.docs
+        .map((doc) => Product(
+              doc.data()['supplier'] ?? '',
+              doc.data()['type'] ?? '',
+              doc.data()['name'] ?? '',
+              doc.data()['price'] ?? '',
+              0,
+            ))
+        .toList();
   }
 }
