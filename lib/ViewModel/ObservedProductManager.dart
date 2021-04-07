090429428e5cefd99c67f0d9224c71856e70f8df
cysko7927 <cysko7927@gmail.com>
2021-04-07 18:51:00 +0200

    BugFix about name of Parameter

--- lib/ViewModel/ObservedProductManager.dart
+++ lib/ViewModel/ObservedProductManager.dart
@@ -66,13 +66,12 @@ class ObservedProductManager extends ChangeNotifier {
   //that contains those products
   //Precondition: The snapshot must contain the products with the correct parameters
   List<Product> _productsObservedListFromSnapshot(QuerySnapshot snapshot) {
-    var a = snapshot.docs.elementAt(0).data()['price'];
     return snapshot.docs
         .map((doc) => Product(
-            doc.data()['supplier'] ?? '',
-            doc.data()['type'] ?? '',
-            doc.data()['name'] ?? '',
-            doc.data()['price'] ?? 0,
+            doc.data()['supplierProduct'] ?? '',
+            doc.data()['typeProduct'] ?? '',
+            doc.data()['nameProduct'] ?? '',
+            0,
             0))
         .toList();
   }
